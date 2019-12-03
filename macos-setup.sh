7b2b52bf4ddad2ff7e1147169fcb5cf5c1260bc5
Maxwell Burson <maxwellburson@gmail.com>
2019-12-02 19:24:40 -0800

    Added macos setup file

---
+++ macos-setup.sh
@@ -0,0 +1,5 @@
+rm -rf ~/Library/Application\ Support/Code/User/settings.json
+ln ./vscode.json ~/Library/Application\ Support/Code/User/settings.json
+
+rm -rf ~/.gitconfig
+ln ./.gitconfig ~/.gitconfig
