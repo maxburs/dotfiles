94ce09942e68929d680ca6a81f00861502756158
Max <maxwellburson@gmail.com>
2021-01-06 15:34:04 -0800

    Update windows-setup.ps1

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy RemoteSigned
 # chocolaty
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
