70f7fe18dc0f1122e29448952a04603e89b186b9
Max <maxwellburson@gmail.com>
2021-02-04 11:57:08 -0800

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -3,3 +3,8 @@ ln ./vscode.json ~/.config/Code/User/settings.json
 
 rm -rf ~/.gitconfig
 ln ./.gitconfig ~/.gitconfig
+
+# Node + Yarn
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+# nvm install --lts
+# npm install --global yarn
