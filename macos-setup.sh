d3a2bccdf95111a074c9eb74a43ccdbae8fb098a
Max <maxwellburson@gmail.com>
2023-03-10 17:27:04 -0800

    Update macos-setup.sh

--- macos-setup.sh
+++ macos-setup.sh
@@ -10,6 +10,15 @@ xcode-select --install
 git config --global credential.helper osxkeychain
 git config --global core.editor "code --wait"
 
+# Make bash default shell
 chsh -s /bin/bash
 
+# Show hidden files in finder
 defaults write com.apple.finder AppleShowAllFiles True
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install visual-studio-code
+brew install iterm2
+brew install google-chrome
+brew install spotify
